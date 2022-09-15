f39f570e6c6a0cf1556df7b83f0c6da5455a46ae
Vitor Silva Reis <vitorejuvian@hotmail.com>
2022-09-15 08:58:13 -0300

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from sheetsAPI import *
 from driveAPI import *
+from shutil import copyfile
 
 
 def main():
@@ -11,24 +12,49 @@ def main():
     else:                                   # Se a pasta onde será armazenado o código não existe então ela será criada
         os.mkdir('LandingPage')
         os.mkdir('LandingPage/img')
+# Site
+    template = int(answers[1])                  # Salva qual template o usuário escolheu
+    siteTitle = answers[2]                      # Salva o titulo do site
+    menuColor = answers[3]                      # Salva a cor do menu
+    cardColor = answers[4]                      # Salva a cor dos cartões de produtos
+    textColor = answers[5]                      # Salva a cor dos textos
+    # backgroundImage = answers[6].split('id=')[1]    # Salva a imagem de fundo do site
+    # imagesIDs.append(backgroundImage)
 
-    template = int(answers[0])                  # Salva qual template o usuário escolheu
-    siteTitle = answers[1]                      # Salva o titulo do site
-    menuColor = answers[2]                      # Salva a cor do menu
-    cardColor = answers[3]                      # Salva a cor dos cartões de produtos
-    textColor = answers[4]                      # Salva a cor dos textos
-    productName = answers[5].split('&')         # Salva o nome de cada produto
-    productPrice = answers[6].split('&')        # Salva o preço de cada produto
-    productDescription = answers[7].split('&')  # Salva a descrição de cada produto
-    whatsapp = answers[8]                       # Salva o número do whatsapp da loja
-    imagesLinks = answers[9].split(',')         # Salva o link das imagens do site
-    imagesIDs = []
-    for image in imagesLinks:                   # Separa os IDs de cada imagem dos links
-        imagesIDs.append(image.split('id=')[1])
+    productNames = []
+    productDescriptions = []
+    productPrices = []
+    productImages = []
 
-    images = catchImages(imagesIDs)
+# Nome dos produtos
+    i = 6
+    while i <= 26:
+        productNames.append(answers[i])
+        i += 4
 
-    # Ajuste de cores de acordo com a resposta
+# Descrição dos produtos
+    i = 7
+    while i <= 27:
+        productDescriptions.append(answers[i])
+        i += 4
+
+# Preço dos produtos
+    i = 8
+    while i <= 28:
+        productPrices.append(answers[i])
+        i += 4
+
+# Imagens dos produtos
+    i = 9
+    while i <= 29:
+        productImages.append(answers[i].split('id=')[1])
+        i += 4
+
+# Informações de contato
+    whatsapp = answers[30]
+    images = catchImages(productImages)
+
+# Ajuste de cores de acordo com a resposta
     if menuColor == 'Azul':
         menuColor = 'bg-primary'
     elif menuColor == 'Cinza':
@@ -81,76 +107,47 @@ def main():
         cardColor = 'black'
 
     if template == 1:
-        template1(siteTitle, menuColor, cardColor, textColor, productName, productPrice, productDescription, whatsapp, images)
+        template1(siteTitle, menuColor, cardColor, textColor, productNames, productPrices, productDescriptions, images, whatsapp)
     elif template == 2:
-        template2(siteTitle, menuColor, cardColor, textColor, productName, productPrice, productDescription, whatsapp, images)
+        template2(siteTitle, menuColor, cardColor, textColor, productNames, productPrices, productDescriptions, images, whatsapp)
 
     print('Sua landing page esta pronta.')
 
-def generate(code):
-    file = open(f'LandingPage/landingPage.html', 'a')
-    file.write(code)
+def template1(siteTitle, menuColor, cardColor, textColor, productNames, productPrices, productDescriptions, images, whatsapp):
+    templatePath = 'template1.html'
+    copyPath = 'LandingPage/landingPage.html'
+    copyfile(templatePath, copyPath)
+    backgroundImage = images[0]
 
-def template1(siteTitle, menuColor, cardColor, textColor, productName, productPrice, productDescription, whatsapp, images):
-    i = 0
-    imageName = []
-    # Separa a imagem de fundo das outras imagens
-    while i < len(images):
-        if images[i].startswith('0'):
-            backgroundImage = images[i]
-        else:
-            imageName.append(images[i])
-        i += 1
+    file = open(f'LandingPage/landingPage.html', 'a+')
 
-    generate('<!DOCTYPE html>\n<html>\n<head>\n<meta name="charset" content="utf-8"/>\n<meta name="viewport" '
-             'content="width=device-width, initial-scale=1.0"/>\n<link rel="stylesheet" '
-             'href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" '
-             'integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">\n'
-             '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">\n'
-             '<!--Espaçamento do container, centralização das linhas e definição da imagem de fundo-->\n<style>\n'
-             '.container-fluid {\npadding-right:0;\npadding-left:0;\nmargin-right:auto;\nmargin-left:auto\n}\n'
-             '.row{\ntext-align: center;\nmargin: auto;\n}\n.bg-image{\n')
+    for line in file:
+        file.write(line.replace('*//siteTitle*//', f'{siteTitle}'))
+        file.write(line.replace('*//menuColor*//', f'{menuColor}'))
+        file.write(line.replace('*//cardColor*//', f'{cardColor}'))
+        file.write(line.replace('*//textColor*//', f'{textColor}'))
+        file.write(line.replace('*//backgroundImage*//', f'{backgroundImage}'))
 
+        for i in range(0, len(productNames)):
+            file.write(line.replace(f'*//productName{i}*//', f'{productNames[i]}'))
 
-    generate(f'background-image: url(./img/{backgroundImage});''\nbackground-repeat: no-repeat;\nbackground-size: cover;\n}\n'
-             '</style>\n')
+        for i in range(0, len(productDescriptions)):
+            file.write(line.replace(f'*//productDescription{i}*//', f'{productDescriptions[i]}'))
 
-    generate(f'<title>{siteTitle}</title>\n</head>\n<body class="bg-image">\n')
+        for i in range(0, len(productPrices)):
+            file.write(line.replace(f'*//productPrice{i}*//', f'{productPrices[i]}'))
 
-    generate(f'<div class="border border-dark {menuColor}" style="width: 50rem; margin: auto; margin-top: 1rem;'
-             f' margin-bottom: 1rem;">\n')
+        for i in range(1, len(images)):
+            file.write(line.replace(f'*//productImage{i}*//', f'{images[i]}'))
 
-    generate(f'<div class="container row border border-dark">\n<h1 class="col text-center" style="color: {textColor};">'
-             f'{siteTitle}</h1>\n</div>\n<br/>\n')
-
-    i = 0
-    for product in productName:
-        generate(f'<div class="container text-center">\n<ul class="list-unstyled">\n<li class="media border'
-            f' border-dark {cardColor}" style="margin: auto;">\n<img class="mr-3" src="./img/{imageName[i]}"'
-            f' alt="Generic placeholder image" style="width: 18rem; height: 13rem; margin: 1rem; border:1px solid black;">\n'
-            f'<div class="media-body my-4">\n<h5 class="mt-0 mb-1" style="color: {textColor};">{productName[i]}</h5>\n'
-            f'<p style="margin-top: 2rem;  color: {textColor};">{productDescription[i]}</p>\n<h5 style="margin-top: 2rem;'
-            f' color: {textColor};">R${productPrice[i]}</h5>\n</div>\n</li>\n</ul>\n</div>\n')
-        i += 1
-
-    generate(f'<div class="row text-center">\n<div class="col text-center border border-dark">\n<h4 '
-        f'style="color: {textColor};">Contato:</h4>\n<div>\n<a href="https://wa.me/55{whatsapp}" target="_blank">\n'
-        f'<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-whatsapp" '
-        f'viewBox="0 0 16 16" style="color: #118011; margin-bottom: 1rem;">\n<path d="M13.601 2.326A7.854 7.854 0 0 0 '
-        f'7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 '
-        f'3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.'
-        f'356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.'
-        f'584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.'
-        f'934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.'
-        f'148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.'
-        f'088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.'
-        f'389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .'
-        f'977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.'
-        f'058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>\n</svg>\n</a>\n</div>\n'
-        f'</div>\n</div>\n</div>\n</body>\n</html>')
+        file.write(line.replace('*//whatsapp*//', f'{whatsapp}'))
 
 def template2(siteTitle, menuColor, cardColor, textColor, productName, productPrice, productDescription, images):
-    print('Template 2')
+    templatePath = 'template2.html'
+    copyPath = 'LandingPage/landingPage.html'
+    copyfile(templatePath, copyPath)
+    backgroundImage = images[0]
+
 
 if __name__ == '__main__':
     main()
